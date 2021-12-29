b2b06b719fd6dc5bbe3e0ed83b256c59950d8b87
Patrick <patrick.ortell@arus.io>
2021-12-28 19:20:22 -0700

    profile images link to posts

--- frontend/src/components/profile/postList/item/index.js
+++ frontend/src/components/profile/postList/item/index.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 export default function ProfilePostListItem({ item }) {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: item.media[1] }} />
+      <TouchableOpacity
+        style={styles.image}
+        onPress={() => {
+          navigation.navigate("postDetails", {
+            videoURLFirebase: item.media[0],
+            imageAssetURLFirebase: item.media[1],
+            userPhotoURL: item.userPhotoURL,
+            postDescription: item.description,
+            postLikesCount: item.likesCount,
+            userName: item.userName,
+            postID: item.id,
+          });
+        }}
+      >
+        <Image style={styles.image} source={{ uri: item.media[1] }} />
+      </TouchableOpacity>
     </View>
   );
 }
