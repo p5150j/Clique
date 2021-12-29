2055894a68b032d52f7d7eaf6ff777b423d8db65
Patrick <patrick.ortell@arus.io>
2021-12-29 11:51:50 -0700

    log out

--- frontend/src/components/profile/navBar/index.js
+++ frontend/src/components/profile/navBar/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import styles from "./styles";
+import firebase from "firebase";
 
 export default function ProfileNavBar({ user }) {
   return (
@@ -10,8 +11,12 @@ export default function ProfileNavBar({ user }) {
         <Feather name="search" size={20} color={"white"} />
       </TouchableOpacity>
       <Text style={styles.text}>{user.displayName}</Text>
-      <TouchableOpacity>
-        <Feather name="menu" size={24} color={"white"} />
+      <TouchableOpacity
+        onPress={() => {
+          firebase.auth().signOut();
+        }}
+      >
+        <Feather name="power" size={24} color={"white"} />
       </TouchableOpacity>
     </View>
   );
